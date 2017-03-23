6e53c3c5c9f4f9199f7fec92f3f0000f07871cc1
Kirill Kulakov <kulakov@cs.karelia.ru>
2017-03-23 14:07:22 +0300

    add command line parser

--- src/main/java/org/fruct/oss/importosmobstacles/Main.java
+++ src/main/java/org/fruct/oss/importosmobstacles/Main.java
@@ -20,21 +20,30 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Map;
 import java.util.logging.Level;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 /**
  *
  * @author kulakov
  */
 public class Main {
-    
-    public static enum Status {ADDED, SKIPPED, UPDATED, MOVED};
+
+    public static enum Status {
+        ADDED, SKIPPED, UPDATED, MOVED
+    };
 
     Connection c = null;
 
     /**
      * Почта суперадмина
      */
-    String publicUser = "getsobstacletest@gmail.com";
+    String publicUser;
 
     /**
      * Идентификатор суперадмина если найден, иначе 0
@@ -44,7 +53,7 @@ public class Main {
     /**
      * Почта пользователя-владельца для импорта
      */
-    String owner = "obstacle.osm@gmail.com";
+    String owner;
 
     /**
      * Идентификатор пользователя-владельца или 0
@@ -53,20 +62,22 @@ public class Main {
 
     HashMap categories = null;
     HashMap categoryNames = null;
-    
+
     Gson gson;
 
-    public Main() throws ObjectNotFoundException {
+    public Main(String _owner, String superUser, String host, int port, String login, String password) throws ObjectNotFoundException {
         categories = new HashMap();
-        categoryNames=  new HashMap();
+        categoryNames = new HashMap();
         gson = new Gson();
+        publicUser = superUser;
+        owner = _owner;
 
         // Соединение с БД
         try {
             Class.forName("org.postgresql.Driver");
             c = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/geo2tag",
-                            "geo2tag", "geo2tag");
+                    .getConnection("jdbc:postgresql://"+host+":"+port+"/geo2tag",
+                            login, password);
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
@@ -103,24 +114,73 @@ public class Main {
 
     public static void main(String[] args) throws ObjectNotFoundException, SQLException {
         /*TODO:  ввод параметров (email'ы, файл с даннными */
+        Options opts = new Options();
+        Option opt = new Option("u", "user", true, "E-mail of owned user");
+        opt.setRequired(false);
+        opts.addOption(opt);
         
-        Main m = new Main();
+        opt = new Option("s", "superuser", true, "E-mail of project super user");
+        opt.setRequired(false);
+        opts.addOption(opt);
+        
+        opt = new Option("i", "input", true, "Input file with obstacles");
+        opt.setRequired(true);
+        opts.addOption(opt);
+        
+        opt = new Option("h", "host", true, "GeTS database server host (default localhost)");
+        opt.setRequired(false);
+        opts.addOption(opt);
+        
+        opt = new Option("p", "port", true, "GeTS database server port (default 5432)");
+        opt.setRequired(false);
+        opts.addOption(opt);
+        
+        opt = new Option("l", "login", true, "GeTS database login (default geo2tag)");
+        opt.setRequired(false);
+        opts.addOption(opt);
+        
+        opt = new Option("w", "password", true, "GeTS database password (default geo2tag)");
+        opt.setRequired(false);
+        opts.addOption(opt);
+        
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+        
+        try {
+            cmd = parser.parse(opts, args);
+        } catch (ParseException ex) {
+            System.err.println(ex.getMessage());
+            formatter.printHelp("importOSMobstacles", opts);
+            System.exit(1);
+            return;
+        }
+        
+        String input_file = cmd.getOptionValue("input");
+        String owner = cmd.getOptionValue("user",  "obstacle.osm@gmail.com");
+        String superUser = cmd.getOptionValue("superuser", "getsobstacletest@gmail.com");
+        String host = cmd.getOptionValue("host", "localhost");
+        int port = Integer.parseInt(cmd.getOptionValue("port", "5432"));
+        String login = cmd.getOptionValue("login", "geo2tag");
+        String password = cmd.getOptionValue("password", "geo2tag");
+        
+        
+        Main m = new Main(owner, superUser, host, port, login, password);
 
         //TODO: разбор файла с данными
-        String input_file = "RU-KR.osm.json";
         long countPoints = 0;
 
         try {
             JsonReader reader = new JsonReader(new FileReader(input_file));
             reader.beginArray();
-            while(reader.hasNext()) {
-                HashMap point = new HashMap<String, Object>();
+            HashMap point = new HashMap<String, Object>();
+            while (reader.hasNext()) {
                 point = m.gson.fromJson(reader, point.getClass());
-                
+
                 Main.Status ret = m.importPoint(point);
-                
+
                 switch (ret) {
-                    case SKIPPED: 
+                    case SKIPPED:
                         System.out.println("Point #" + countPoints + " ID=" + ((Double) point.get("id")).intValue() + " was skipped");
                         break;
                     case ADDED:
@@ -135,7 +195,7 @@ public class Main {
                 }
                 countPoints++;
             }
-            
+
 //        HashMap point = new HashMap();
 //        point.put("id", 365583834);
 //        point.put("type", "traffic_signals");
@@ -145,7 +205,6 @@ public class Main {
 //        point.put("bicycle", "yes");
 //        point.put("highway", "traffic_signals");
 //        point.put("crossing", "traffic_signals");
-
 //        if (m.importPoint(point)) {
 //            Logger.getLogger("logger").info("point " + point.get("id") + " was added/updated");
 //        } else {
@@ -175,8 +234,8 @@ public class Main {
         if (category == 0) {
             throw new ObjectNotFoundException("Category \"" + point.get("type") + "\" not found.");
         }
-        
-        if (((Double)point.get("id")).intValue() == 1831963699) {
+
+        if (((Double) point.get("id")).intValue() == 1831963699) {
             System.out.println("Bingo");
         }
 
@@ -188,17 +247,17 @@ public class Main {
                 + ") < 0.00001 and tag.user_id=" + ownerID + " and tag.channel_id=channel.id and channel.description like '%\"category_id\":\"" + category + "\"%';";
 
         ResultSet rs = stmt.executeQuery(query);
-        if(rs.last()) {
+        if (rs.last()) {
             int rows = rs.getRow();
             if (rows > 1) {
                 //TODO: реализовать обработку нескольких результатов
                 throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
             }
-            
+
             int point_id = rs.getInt("id");
             String label = rs.getString("label");
             String description = rs.getString("description");
-            
+
             if (validatePoint(point, label, description)) {
                 return Status.SKIPPED;
             } else {
@@ -216,7 +275,12 @@ public class Main {
                     + " and tag.channel_id=channel.id and tag.user_id <> " + ownerID + " and channel.description like '%\"category_id\":\"" + category + "\"%';";
             //System.out.println(query);
             rs = stmt.executeQuery(query);
-            while (rs.next()) {
+            if (rs.last()) {
+                int rows = rs.getRow();
+                if (rows > 1) {
+                    //TODO: реализовать обработку нескольких результатов
+                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
                 int point_id = rs.getInt("id");
                 int user_id = rs.getInt("user_id");
                 String label = rs.getString("label");
@@ -251,8 +315,9 @@ public class Main {
         ResultSet rs = stmt.executeQuery(query);
 
         if (rs.last()) {
-            if (rs.getRow() > 1)
+            if (rs.getRow() > 1) {
                 throw new SQLException("Multiple categories with osmkey=" + key);
+            }
             categories.put(key, rs.getInt("id"));
             categoryNames.put(rs.getInt("id"), rs.getString("name"));
         } else {
@@ -279,8 +344,9 @@ public class Main {
 
         int channel = 0;
         if (rs.last()) {
-            if (rs.getRow() > 1)
-                throw new ObjectNotFoundException("Can't find channel for category id="+category);
+            if (rs.getRow() > 1) {
+                throw new ObjectNotFoundException("Can't find channel for category id=" + category);
+            }
             channel = rs.getInt("id");
         }
         stmt.close();
@@ -291,8 +357,8 @@ public class Main {
         System.out.println("Found channel " + channel);
 
         //TODO: исправление содержания label и description
-        Map<String,Object> descriptionMap = new HashMap<String,Object>();
-        descriptionMap = (Map<String,Object>) gson.fromJson(description, descriptionMap.getClass());
+        Map<String, Object> descriptionMap = new HashMap<String, Object>();
+        descriptionMap = (Map<String, Object>) gson.fromJson(description, descriptionMap.getClass());
         if (descriptionMap.containsKey("nodeID")) {
             descriptionMap.put("osmID", descriptionMap.get("nodeID"));
             descriptionMap.remove("nodeID");
@@ -302,7 +368,7 @@ public class Main {
         }
         description = gson.toJson(descriptionMap);
         //System.out.println("new description: " + description);
-        
+
         query = "update tag set label=?, description=?, user_id=?, channel_id=? where id=?;";
         PreparedStatement pstmt = c.prepareStatement(query);
         pstmt.setString(1, label);
@@ -314,18 +380,19 @@ public class Main {
             throw new SQLException("Point ID=" + pointID + " not updated");
         }
     }
-    
+
     private boolean addPoint(HashMap point, int category) throws SQLException, ObjectNotFoundException {
         Statement stmt = c.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        
+
         // Ищем наш канал для нужной категории
         String query = "select id from channel where owner_id=" + ownerID + " and channel.description like '%\"category_id\":\"" + category + "\"%';";
         ResultSet rs = stmt.executeQuery(query);
 
         int channel = 0;
         if (rs.last()) {
-            if (rs.getRow() > 1)
-                throw new ObjectNotFoundException("Can't find channel for category id="+category);
+            if (rs.getRow() > 1) {
+                throw new ObjectNotFoundException("Can't find channel for category id=" + category);
+            }
             channel = rs.getInt("id");
         }
         stmt.close();
@@ -334,7 +401,7 @@ public class Main {
             channel = createChannel(category);
         }
         System.out.println("addPoint(): Found channel " + channel);
-        
+
         // добавляем точку
         HashMap descriptionMap = new HashMap();
         descriptionMap.put("osmID", ((Double) point.get("id")).intValue());
@@ -342,7 +409,7 @@ public class Main {
         descriptionMap.put("description_ru", "Загружено с сервиса Openstreetmap");
         descriptionMap.put("rating", 1);
         String description = gson.toJson(descriptionMap);
-        
+
         query = "insert into tag (label, description, channel_id, user_id, latitude, longitude, altitude, url) values (?,?,?,?,?,?,?,?);";
         PreparedStatement pstmt = c.prepareStatement(query);
         pstmt.setString(1, (String) categoryNames.get(category));
@@ -354,7 +421,7 @@ public class Main {
         pstmt.setDouble(7, 0.0);
         pstmt.setString(8, "{}");
         //TODO: добавить нужные поля!!!!
-        
+
         if (pstmt.executeUpdate() != 1) {
             throw new SQLException("Point ID=" + point.get("id") + " not inserted");
         }
@@ -377,8 +444,10 @@ public class Main {
     }
 
     /**
-     * Проверка валидности точки (наличия всех полей корректно заполненных). 
-     * Сейчас проверяются устаревшее поле nodeID (должно быть osmID) и наличие рейтинга
+     * Проверка валидности точки (наличия всех полей корректно заполненных).
+     * Сейчас проверяются устаревшее поле nodeID (должно быть osmID) и наличие
+     * рейтинга
+     *
      * @param point данные о точке из OSM
      * @param label название точки в GeTS
      * @param description описание точки в GeTS
@@ -386,7 +455,7 @@ public class Main {
      */
     private boolean validatePoint(HashMap point, String label, String description) {
         if (description.contains("\"nodeID\":\"" + point.get("id").toString() + "\"")
-                        || !description.contains("\"rating\"")) {
+                || !description.contains("\"rating\"")) {
             return false;
         } else {
             return true;
